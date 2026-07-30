05d9132f90eecebb1eeceba86f0471a6248f34df
Vidushi Saini <work.vidushi.s@gmail.com>
2026-07-30 23:57:33 +0530

    Level - 3, exercise - 9 completed

--- practice/level3.py
+++ practice/level3.py
@@ -174,25 +174,270 @@ pd_df = pd.read_parquet("data\\yellow_tripdata_2025-01.parquet")
 
 #! PANDAS
 
+
 import numpy as np
 
-new_cols = (
-    pd_df.assign(
-        trip_duration_minutes = ((pd_df["tpep_pickup_datetime"] - pd_df["tpep_dropoff_datetime"]).dt.total_seconds() / 60),
-        avg_speed = ((pd_df["trip_distance"] * 1.609) / ((pd_df["tpep_pickup_datetime"] - pd_df["tpep_dropoff_datetime"]).dt.total_seconds() / 3600)),
-        fare_per_mile = (pd_df["trip_distance"] / pd_df["fare_amount"]).round(2),
-        tip_percentage = ((pd_df["tip_amount"] / pd_df["fare_amount"]) * 100).round(2),
-        is_airport_trip = np.where(
-            pd_df["Airport_fee"] != 0, True, False
-        ),
-        is_weekend = np.where(
-            pd_df["tpep_pickup_datetime"].dt.weekday.isin([5, 6]), True, False
-        ), 
-        is_night_trip = np.where(
-            ((pd_df["tpep_pickup_datetime"].dt.hour >= 22) | (pd_df["tpep_pickup_datetime"].dt.hour < 6)), True, False
-        )
-    )
+# new_cols = (
+#     pd_df.assign(
+#         trip_duration_minutes = ((pd_df["tpep_pickup_datetime"] - pd_df["tpep_dropoff_datetime"]).dt.total_seconds() / 60),
+#         avg_speed = ((pd_df["trip_distance"] * 1.609) / ((pd_df["tpep_pickup_datetime"] - pd_df["tpep_dropoff_datetime"]).dt.total_seconds() / 3600)),
+#         fare_per_mile = (pd_df["trip_distance"] / pd_df["fare_amount"]).round(2),
+#         tip_percentage = ((pd_df["tip_amount"] / pd_df["fare_amount"]) * 100).round(2),
+#         is_airport_trip = np.where(
+#             pd_df["Airport_fee"] != 0, True, False
+#         ),
+#         is_weekend = np.where(
+#             pd_df["tpep_pickup_datetime"].dt.weekday.isin([5, 6]), True, False
+#         ),
+#         is_night_trip = np.where(
+#             ((pd_df["tpep_pickup_datetime"].dt.hour >= 22) | (pd_df["tpep_pickup_datetime"].dt.hour < 6)), True, False
+#         )
+#     )
+# )
+
+# print(new_cols)
+
+
+# * Exercise 9 - Business Questions
+
+#! DUCKDB
+
+# Ques 1 - Which Vendor earns more
+
+# vendor_with_more_earning = duckdb.sql("""
+#     SELECT VendorID, ROUND(SUM(total_amount), 2) AS total_earning FROM taxi_data
+#     GROUP BY VendorID
+#     ORDER BY total_earning DESC
+#     LIMIT 1;
+# """)
+
+# print(vendor_with_more_earning)
+
+
+# Ques 2 - Which Payment type gives highest tips
+
+# payment_type_with_highest_tip = duckdb.sql("""
+#     SELECT payment_type, MAX(tip_amount) AS highest_tip FROM taxi_data
+#     GROUP BY payment_type
+#     ORDER BY highest_tip DESC
+#     LIMIT 1;
+# """)
+
+# print(payment_type_with_highest_tip)
+
+
+# Ques 3 - Average Revenue per trip
+
+# avg_revenue_per_trip = duckdb.sql("""
+#     SELECT ROUND((SUM(total_amount) / COUNT(*)), 2) AS avg_revenue_per_trip FROM taxi_data
+# """)
+
+# print(avg_revenue_per_trip)
+
+
+# Ques 4 - Most common pickup location
+
+# most_common_pu_location = duckdb.sql("""
+#     SELECT COUNT(*) AS count_of_location, PULocationID FROM taxi_data
+#     GROUP BY PULocationID
+#     ORDER BY count_of_location DESC
+#     LIMIT 1;
+# """)
+
+# print(most_common_pu_location)
+
+
+# Ques 5 - Most common dropoff location
+
+# most_common_do_location = duckdb.sql("""
+#     SELECT COUNT(*) AS count_of_location, DOLocationID    FROM taxi_data
+#     GROUP BY DOLocationID
+#     ORDER BY count_of_location DESC
+#     LIMIT 1;
+# """)
+
+# print(most_common_do_location)
+
+
+# Ques 6 - Airport trips vs non-airport trips
+
+# airport_vs_non_airport_trip = duckdb.sql("""
+#     SELECT
+#         CASE
+#          WHEN Airport_fee != 0 THEN TRUE
+#          ELSE FALSE
+#          END AS is_airport_trip,
+#          COUNT(*) AS total_trips
+#            FROM taxi_data
+#     GROUP BY is_airport_trip
+# """)
+
+# print(airport_vs_non_airport_trip)
+
+
+# Ques 7 - Longest Average trip distance
+
+# longest_trip_distance = duckdb.sql("""
+#     SELECT MAX(trip_distance) AS longest_trip_diance FROM taxi_data
+
+# """)
+
+# print(longest_trip_distance)
+
+
+# Ques 8 - Highest Average fare
+
+# higest_fare = duckdb.sql("""
+#     SELECT MAX(fare_amount) AS highest_avg_fare FROM taxi_data
+
+# """)
+
+# print(higest_fare)
+
+
+#! POLARS
+
+
+# Ques 1 - Which Vendor earns more
+
+# most_earning_vendor = pl_df.group_by("VendorID").agg(
+#     pl.col('total_amount').sum().alias('total_earning')
+# ).sort('total_earning', descending=True).head(1)
+
+# print(most_earning_vendor)
+# print(most_earning_vendor.to_pandas())
+
+
+# Ques 2 - Which payment type gives highest tip
+
+# highest_tip_paying_payment_type = pl_df.group_by("payment_type").agg(
+#     pl.col('tip_amount').max().alias('highest_tip')
+# ).sort('highest_tip', descending=True).head(1)
+
+# print(highest_tip_paying_payment_type)
+
+
+# Ques 3 - Average revenue per trip
+
+# avg_revenue_per_trip = pl_df.select(
+#     pl.col('total_amount').mean().round(2).alias("avg_revenue_per_trip")
+# )
+
+# print(avg_revenue_per_trip)
+
+
+# Ques 4 - Most common pickup location
+
+# most_common_pu_location = pl_df.group_by('PULocationID').agg(
+#     pl.col('trip_distance').count().alias("count_of_trips")
+# ).sort('count_of_trips', descending=True).head(1)
+
+# print(most_common_pu_location)
+
+
+# Ques 5 - Most common dropoff location
+
+# most_common_do_location = pl_df.group_by('DOLocationID').agg(
+#     pl.len().alias("count_of_trips")
+# ).sort('count_of_trips', descending=True).head(1)
+
+# print(most_common_do_location)
+
+
+# Ques 6 - Airport vs non- airport trip
+
+# airport_vs_non_airport_trip = pl_df.group_by(
+#     (pl.col("Airport_fee") != 0).alias("is_airport_trip")
+# ).agg(pl.len().alias("total_trips"))
+
+# print(airport_vs_non_airport_trip)
+
+
+# Ques 7 - Longest trip distance
+
+# longest_trip_distance = pl_df.select(
+#         pl.col('trip_distance').max().alias('longest_trip_distance')
+#       )
+
+# print(longest_trip_distance)
+
+
+# Ques 8 - Highest Avg fare
+
+# highest_avg_fare = pl_df.select(
+#         pl.col('fare_amount').max().alias('highest_avg_fare')
+#       )
+
+# print(highest_avg_fare)
+
+
+#! PANDAS
+
+
+# Ques 1 - Which Vendor earns more
+
+
+# most_earning_vendor = (
+#     pd_df
+#     .groupby("VendorID")["total_amount"]
+#     .sum()
+#     .reset_index(name="total_earning")
+#     .sort_values('total_earning', ascending=False)
+#     .head(1)
+# )
+
+# print(most_earning_vendor)
+
+
+# Ques 2 - Highest tip paying payment type
+
+
+# highest_tip_paying_payment_type = (
+#     pd_df
+#     .groupby("payment_type")["tip_amount"]
+#     .max()
+#     .reset_index(name="total_tip")
+#     .sort_values('total_tip', ascending=False)
+#     .head(1)
+# )
+
+# print(highest_tip_paying_payment_type)
+
+
+# Ques 3 - AVG revenue per trip
+
+
+# avg_revenue_per_trip = pd_df['total_amount'].mean()
+
+# print(avg_revenue_per_trip)
+
+
+
+# Ques 4 - Most common pickup location
+
+
+# most_common_pu_location = (
+#     pd_df
+#     .groupby("PULocationID")["PULocationID"]
+#     .count()
+#     .reset_index(name="total_trips")
+#     .sort_values('total_trips', ascending=False)
+#     .head(1)
+# )
+
+# print(most_common_pu_location)
+
+
+# Ques 5 - Most common dropoff location
+
+
+most_common_do_location = (
+    pd_df
+    .groupby("DOLocationID")["DOLocationID"]
+    .count()
+    .reset_index(name="total_trips")
+    .sort_values('total_trips', ascending=False)
+    .head(1)
 )
 
-print(new_cols)
-
+print(most_common_do_location)
