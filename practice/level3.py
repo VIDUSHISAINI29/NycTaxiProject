725a846bf4d14176f738ce0329612a838e182efc
Vidushi Saini <work.vidushi.s@gmail.com>
2026-07-30 21:50:35 +0530

    Level - 3, exercise - 8 completed.

--- practice/level3.py
+++ practice/level3.py
@@ -104,7 +104,95 @@ pd_df = pd.read_parquet("data\\yellow_tripdata_2025-01.parquet")
 # print(avg_fare_by_hour)
 
 
-
-
 # * Exercise 8 - Trip Duration
+
+# Create new coloumns trip_duratin_minutes, average_speed, fare_per_mile, tip_percentage_, is_Weekend, is_night_trip, is_airport_trip
+
+#! DUCKDB
+
+# new_cols = duckdb.sql("""
+#         SELECT *,
+#        DATEDIFF('minute', tpep_pickup_datetime, tpep_dropoff_datetime) AS trip_duration_minutes,
+#        ROUND((DATEDIFF('minute', tpep_pickup_datetime, tpep_dropoff_datetime) / 60) / (trip_distance * 1.609), 2) AS avg_speed_km_per_hr,
+#        ROUND((fare_amount / trip_distance), 2) AS fare_per_mile,
+#        ROUND(((tip_amount / fare_amount) * 100), 2) AS tip_percentage,
+#        Airport_fee != 0 AS is_airport_trip,
+#        CASE
+#         WHEN DAYOFWEEK(tpep_pickup_datetime) IN (0, 6) THEN TRUE
+#         ELSE FALSE
+#         END AS is_weekend,CASE
+#         WHEN EXTRACT(HOUR FROM tpep_pickup_datetime) >= 22
+#          OR EXTRACT(HOUR FROM tpep_pickup_datetime) < 6
+#         THEN TRUE
+#         ELSE FALSE
+#         END AS is_night_trip
+#             FROM taxi_data
+#         WHERE tpep_pickup_datetime < tpep_dropoff_datetime
+#         LIMIT 5;
+# """)
+
+# print(new_cols)
+
+
+#! POLARS
+
+
+# new_cols = pl_df.select(
+#     (pl.col("tpep_dropoff_datetime") - pl.col("tpep_pickup_datetime"))
+#     .dt.total_minutes()
+#     .alias("trip_duration_minutes"),
+#     (
+#         (pl.col("trip_distance") * 1.609)
+#         / (
+#             (
+#                 pl.col("tpep_dropoff_datetime") - pl.col("tpep_pickup_datetime")
+#             ).dt.total_minutes()
+#             / 60
+#         )
+#     )
+#     .round(2)
+#     .alias("average_speed_km_per_hour"),
+#     (pl.col("fare_amount") / pl.col("trip_distance")).round(2).alias("fare_per_mile"),
+#     ((pl.col("tip_amount") / pl.col("fare_amount")) * 100)
+#     .round(2)
+#     .alias("tip_percentage"),
+#     pl.when(pl.col("tpep_pickup_datetime").dt.weekday().is_in([5, 6]))
+#     .then(True)
+#     .otherwise(False)
+#     .alias("is_weekend"),
+#     pl.when(
+#         pl.col("Airport_fee") != 0
+#     )
+#     .then(True)
+#     .otherwise(False)
+#     .alias("is_airport_trip"),
+#     ((pl.col('tpep_pickup_datetime').dt.hour() >= 22) | (pl.col('tpep_pickup_datetime').dt.hour() < 6)).alias('is_night_trip')
+# )
+
+# print(new_cols)
+
+
+#! PANDAS
+
+import numpy as np
+
+new_cols = (
+    pd_df.assign(
+        trip_duration_minutes = ((pd_df["tpep_pickup_datetime"] - pd_df["tpep_dropoff_datetime"]).dt.total_seconds() / 60),
+        avg_speed = ((pd_df["trip_distance"] * 1.609) / ((pd_df["tpep_pickup_datetime"] - pd_df["tpep_dropoff_datetime"]).dt.total_seconds() / 3600)),
+        fare_per_mile = (pd_df["trip_distance"] / pd_df["fare_amount"]).round(2),
+        tip_percentage = ((pd_df["tip_amount"] / pd_df["fare_amount"]) * 100).round(2),
+        is_airport_trip = np.where(
+            pd_df["Airport_fee"] != 0, True, False
+        ),
+        is_weekend = np.where(
+            pd_df["tpep_pickup_datetime"].dt.weekday.isin([5, 6]), True, False
+        ), 
+        is_night_trip = np.where(
+            ((pd_df["tpep_pickup_datetime"].dt.hour >= 22) | (pd_df["tpep_pickup_datetime"].dt.hour < 6)), True, False
+        )
+    )
+)
+
+print(new_cols)
 
